06142a45a8d5037bd130b3e10669ec2c3f9a7ce7
ricardo144 <48858998+ricardo144@users.noreply.github.com>
2019-03-30 11:10:03 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+using System.Collections.Generic;
+using Microsoft.Rest;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        /// VOICE RECOGNIZATION     
+        static async Task<string> RecognizeSpeechAsync()
+        {
+            // Creates an instance of a speech config with specified subscription key and service region.
+            // Replace with your own subscription key and service region (e.g., "westus").
+            var config = SpeechConfig.FromSubscription("Subscription_key_speech", "westus");
+
+            // Creates a speech recognizer.
+            using (var recognizer = new SpeechRecognizer(config))
+            {
+                Console.WriteLine("Say something to your microphone,we will analyse your emotion.");
+
+                // Starts speech recognition, and returns after a single utterance is recognized. The end of a
+                // single utterance is determined by listening for silence at the end or until a maximum of 15
+                // seconds of audio is processed.  The task returns the recognition text as result. 
+                // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
+                // shot recognition like command or query. 
+                // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
+                var result = await recognizer.RecognizeOnceAsync();
+
+                // Checks result.
+                if (result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    Console.WriteLine($"We recognized: {result.Text}");
+
+
+                }
+                else if (result.Reason == ResultReason.NoMatch)
+                {
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                }
+                else if (result.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                    }
+                }
+                string x = result.Text;
+                return x;
+            }
+
+        }
+
+        /// TEXT ANALYSIS
+        /// Container for subscription credentials. Make sure to enter your valid key.
+        private const string SubscriptionKey = "Subscription_key_cognitive"; //Insert your Text Anaytics subscription key
+
+        /// </summary>
+        class ApiKeyServiceClientCredentials : ServiceClientCredentials
+        {
+            public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                request.Headers.Add("Ocp-Apim-Subscription-Key", SubscriptionKey);
+                return base.ProcessHttpRequestAsync(request, cancellationToken);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Create a client.
+            ITextAnalyticsClient client = new TextAnalyticsClient(new ApiKeyServiceClientCredentials())
+            {
+                Endpoint = "https://westus.api.cognitive.microsoft.com"
+            }; //Replace 'westus' with the correct region for your Text Analytics subscription
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+
+
+            // Extracting sentiment
+            Console.WriteLine("\n\n===== SENTIMENT ANALYSIS ======");
+
+
+            var extractedtext = RecognizeSpeechAsync();
+
+            string text = extractedtext.Result;
+
+            SentimentBatchResult result3 = client.SentimentAsync(
+                    new MultiLanguageBatchInput(
+                        new List<MultiLanguageInput>()
+                        {
+
+                          new MultiLanguageInput("en", "0",text),
+
+                        })).Result;
+
+
+            // Printing sentiment results
+            foreach (var document in result3.Documents)
+            {
+
+                Console.WriteLine($"Sentiment Score: {document.Score:0.00} of 1.00");
+                Console.WriteLine($"Higher score you get, more positive your speech is. ");
+                if (document.Score > 0.5)
+                {
+                    Console.WriteLine($"RESULT: The speech may be positive.");
+                }
+                else if (document.Score == 0.5)
+                {
+                    Console.WriteLine($"RESULT: The speech may be neuter.");
+                }
+                else
+                {
+                    Console.WriteLine($"RESULT: The speech may be negative.");
+                }
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
